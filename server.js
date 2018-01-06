65bcd610266209117875509487f33e2c1a475293
Jordan Irabor <vastcastiel@gmail.com>
2018-01-06 23:46:38 +0100

    built for github pages

---
+++ server.js
@@ -0,0 +1,9 @@
+
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('server started '+ port);
